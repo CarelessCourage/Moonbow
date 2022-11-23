ea17052706db0fea33232ec26124ddc20f218c54
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2022-11-24 00:25:33 +0100

    experiment withTS package configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import glsl from 'vite-plugin-glsl'
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), glsl()],
+  plugins: [vue(), glsl(), dts()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
